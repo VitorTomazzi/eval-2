011b0228d131e888cbb03c2949dfe1c3c24fd0ec
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-12-15 22:45:21 -0500

    again

--- client/src/components/question.jsx
+++ client/src/components/question.jsx
@@ -28,7 +28,7 @@ export default class Question extends Component {
 					min="1"
 					max="5"
 					style={{
-						width: '13%',
+						width: '18%',
 						backgroundColor: '#E4FDE0',
 						color: '#114B5F',
 						borderStyle: 'none',
